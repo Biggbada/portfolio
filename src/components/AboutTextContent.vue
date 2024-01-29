1d6bfb3b27108c40bf22a87006898ea4175bb7be
Biggbada <yguenot@polepo.fr>
2024-01-29 22:08:06 +0100

    css fixes and about section first content

--- src/components/AboutTextContent.vue
+++ src/components/AboutTextContent.vue
@@ -2,15 +2,21 @@
   <div id="bio-col-2" class="item-column bordered">
     <div id="bio-content" class="code">
       /**<br />
-      1 * About me
-      <br />2 * I have 5 years of experience in web <br />3 * development lorem ipsum dolor sit
-      amet, <br />4 * consectetur adipiscing elit, sed do eiusmod <br />5 * tempor incididunt ut
-      labore et dolore <br />6 * magna aliqua. Ut enim ad minim veniam, <br />7 * quis nostrud
-      exercitation ullamco laboris <br />8 * nisi ut aliquip ex ea commodo consequat. <br />9 * Duis
-      aute irure dolor in reprehenderit in <br />10 * <br />11 * Duis aute irure dolor in
-      reprehenderit in <br />12 * voluptate velit esse cillum dolore eu fugiat <br />13 * nulla
-      pariatur. Excepteur sint occaecat <br />14 * officia deserunt mollit anim id est laborum.
-      <br />15 */
+      1 * About me<br />
+      2 * -------- <br />
+      3 * Hello World<br />
+      4 * My name is Yann GUENOT<br />
+      5 * I'm a french web dev.<br />
+      6 * working as a freelance<br />
+      7 * -----------------<br />
+      8 * Wanna know more ?<br />
+      9 * Discover some projects <br />
+      10 * samples and contact <br />
+      11 * me on contact page !<br />
+      12 * You also can visit<br />
+      13 * my GitHub or Linkedin<br />
+      14 * pages<br />
+      15 */
     </div>
   </div>
 </template>
