8981bcdf149068f030068d8e81988f3f96343dcf
Biggbada <yguenot@polepo.fr>
2024-04-12 23:40:34 +0200

    added click area in nav items

--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -5,13 +5,12 @@ import { useStore } from '@/stores/store'
 const store = useStore()
 const mediaQuery = store.mql
 console.log(mediaQuery)
-
 </script>
 <script>
 export default {
-    components: {
-        Slide // Register your component
-    }
+  components: {
+    Slide // Register your component
+  }
 }
 </script>
 <template>
@@ -21,14 +20,14 @@ export default {
         <Slide noOverlay :closeOnNavigation="true">
           <ul>
             <div id="navbar-container" class="flex-row-start">
-              <li><RouterLink to="/">yann-guenot</RouterLink></li>
-              <li><RouterLink to="/about">_about</RouterLink></li>
-              <li><RouterLink to="/projects">_projects</RouterLink></li>
+              <RouterLink to="/"><li>yann-guenot</li></RouterLink>
+              <RouterLink to="/about"><li>_about</li></RouterLink>
+              <RouterLink to="/projects"><li>_projects</li></RouterLink>
               <!-- <li><a href="#">_incoming</a></li>
               <li><a href="#">_delivered</a></li>
               <li><a href="#">_pricing</a></li>
               <li><a href="#">_recent</a></li> -->
-              <li id="menu-contact"><RouterLink to="/contact">_contact</RouterLink></li>
+              <RouterLink to="/contact"><li id="menu-contact">_contact</li></RouterLink>
               <!-- <li id="menu-login"><a href="#">_login</a></li> -->
             </div>
           </ul>
@@ -37,14 +36,14 @@ export default {
       <div v-else>
         <ul>
           <div id="navbar-container" class="flex-row-start">
-            <li><RouterLink to="/">yann-guenot</RouterLink></li>
-            <li><RouterLink to="/about">_about</RouterLink></li>
-            <li><RouterLink to="/projects">_projects</RouterLink></li>
+            <RouterLink to="/"><li>yann-guenot</li></RouterLink>
+            <RouterLink to="/about"><li>_about</li></RouterLink>
+            <RouterLink to="/projects"><li>_projects</li></RouterLink>
             <!-- <li><a href="#">_incoming</a></li>
             <li><a href="#">_delivered</a></li>
             <li><a href="#">_pricing</a></li>
             <li><a href="#">_recent</a></li> -->
-            <li id="menu-contact"><RouterLink to="/contact">_contact</RouterLink></li>
+            <RouterLink to="/contact" id="menu-contact"><li>_contact</li></RouterLink>
             <!-- <li id="menu-login"><a href="#">_login</a></li> -->
           </div>
         </ul>
