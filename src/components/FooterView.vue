0eac9200eabb2af54893678194a0f18c2ca4f205
Biggbada <yguenot@polepo.fr>
2024-04-16 08:26:17 +0200

    minor css improvements on mobile footer display - still a scroll to fix

--- src/components/FooterView.vue
+++ src/components/FooterView.vue
@@ -6,22 +6,27 @@
           <li id="find-me" class="noli">find me in</li>
           <a href="https://www.linkedin.com/in/yann-guenot-607171101/" id="linkedin-link">
             <li class="noli">
-              <img
-                id="logo-linkedin"
-                src="/assets/logo-linkedin.svg"
-                width="16"
-                height="16"
-                alt=""
-              />_yann_guenot
+              <span>
+                <img
+                  id="logo-linkedin"
+                  src="/assets/logo-linkedin.svg"
+                  width="16"
+                  height="16"
+                  alt=""
+                />
+              </span>
+              <span> _yann_guenot </span>
             </li>
           </a>
           <li id="central-footer-li">-</li>
-          <li class="noli">
-            <a id="git-link" href="https://github.com/Biggbada">
-              <img id="logo-git" src="/assets/github-logo.svg" width="16" height="16" alt="" />
-              _@Biggbada
-            </a>
-          </li>
+          <a id="git-link" href="https://github.com/Biggbada">
+            <li class="noli">
+              <span
+                ><img id="logo-git" src="/assets/github-logo.svg" width="16" height="16" alt=""
+              /></span>
+              <span>_@Biggbada</span>
+            </li>
+          </a>
         </div>
       </ul>
     </div>
