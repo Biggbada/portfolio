3205b84424d0a5a9556dba592064bd051d6aa1c2
Biggbada <yguenot@polepo.fr>
2024-04-17 08:11:19 +0200

    no more horizontal scroll on mobile version - fixed a typo on about page div id

--- src/views/LayoutView.vue
+++ src/views/LayoutView.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="about-main">
     <slot name="subheader"></slot>
-    <div id="about-desription" class="flex-row-start">
+    <div id="about-description" class="flex-row-start">
       <slot name="aside"></slot>
       <slot name="content"></slot>
     </div>
