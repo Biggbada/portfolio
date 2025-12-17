e0ab7acfb8691c2c21678420f2a4b94996605bfa
Yann G. <yguenot@polepo.fr>
2025-12-17 21:51:23 +0100

    Add crypto.getRandomValues polyfill for Node build

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,21 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { randomBytes } from 'crypto'
 
 // https://vitejs.dev/config/
+
+
+// Polyfill global crypto.getRandomValues pour Node
+if (!globalThis.crypto) {
+  globalThis.crypto = {}
+}
+globalThis.crypto.getRandomValues = (arr) => {
+  const bytes = randomBytes(arr.length)
+  arr.set(bytes)
+  return arr
+}
+
 export default defineConfig({
   base: '/',
   plugins: [vue()],
